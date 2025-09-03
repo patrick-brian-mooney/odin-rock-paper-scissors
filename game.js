fd1cea5f07b538f9898ccb8c745d5ab6857b830d
Patrick Mooney <github.pmooney78@neverbox.com>
2025-09-03 14:49:46 -0500

    Add function described in step 2 of instructions

--- game.js
+++ game.js
@@ -1,1 +1,11 @@
-console.log('Hello world')
+function getComputerChoice() {
+    if (Math.random() <= (1/3)) {
+        return "rock"
+    } else if (Math.random() <= (1/2)) {
+        return "paper"
+    } else {
+        return "scissors"
+    }
+}
+
+console.log('... JavaScript code loaded!')
