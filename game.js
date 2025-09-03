0cf44eb1f53700d6b73bdb4853329931623ac7ad
Patrick Mooney <github.pmooney78@neverbox.com>
2025-09-03 14:56:33 -0500

    Add function described in step 3 of instructions

--- game.js
+++ game.js
@@ -8,4 +8,17 @@ function getComputerChoice() {
     }
 }
 
+function getHumanChoice() {
+    let attempts = 0
+    let resp = null
+
+    while (resp != 'rock' && resp != 'paper' & resp != 'scissors') {
+        resp = prompt((attempts > 0) ? 'That is not a valid choice. Rock, paper, or scissors?' : 'What move do you make?')
+        resp = resp.trim().toLowerCase()
+        attempts++
+    }
+
+    return resp
+}
+
 console.log('... JavaScript code loaded!')
